7c31ced26dacc02a0fd5cf03f71bccdbecee9539
ManishReddyK <manish.koppula@iiitb.ac.in>
2023-10-31 15:52:29 +0530

    Commiting the Code for Calculator

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,39 @@
+//package org.example;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.Before;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class MainTest {
+//
+//    private Main testCalc;
+//
+//    @BeforeAll
+//    public void setUp() { testCalc = new Main();    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//    }
+//
+//    @Test
+//    public void squareRoot() {
+//    }
+//
+//    @Test
+//    public void factorial() {
+//    }
+//
+//    @Test
+//    public void naturalLog() {
+//    }
+//
+//    @Test
+//    public void power() {
+//    }
+//
+//    public Main getTestCalc() {
+//        return testCalc;
+//    }
+//}
