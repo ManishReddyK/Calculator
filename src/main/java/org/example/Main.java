d49ce2810e42e8167867d76c03f5f88fe6637920
ManishReddyK <manish.koppula@iiitb.ac.in>
2023-11-05 11:39:41 +0530

    Modified the Code for Calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,15 +17,15 @@ public class Main {
         do {
             System.out.println("--------------Calculator--------------");
             System.out.println("Choices of Operations: ");
-            System.out.println("");
+            System.out.println();
             System.out.println("1. Square root");
             System.out.println("2. Factorial");
             System.out.println("3. Natural Log");
             System.out.println("4. Power");
             System.out.println("5. Exit");
-            System.out.println("");
+            System.out.println();
             System.out.print("Enter your choice (number): ");
-            System.out.println("");
+            System.out.println();
             op = reader.nextInt();
             if (op == 5) flag = 1;
             else {
@@ -33,7 +33,7 @@ public class Main {
                     case 1:
                         logger.info("Start of Execution");
                         System.out.println("You choose Square Root!");
-                        System.out.println("");
+                        System.out.println();
                         System.out.print("Enter Number: ");
                         num = reader.nextDouble();
                         squareRoot(num);
@@ -43,7 +43,7 @@ public class Main {
                     case 2:
                         logger.info("Start of Execution");
                         System.out.println("You choose Factorial!");
-                        System.out.println("");
+                        System.out.println();
                         System.out.print("Enter Number: ");
                         numm = reader.nextInt();
                         factorial(numm);
@@ -53,7 +53,7 @@ public class Main {
                     case 3:
                         logger.info("Start of Execution");
                         System.out.println("You choose natural Log!");
-                        System.out.println("");
+                        System.out.println();
                         System.out.print("Enter Number: ");
                         num = reader.nextDouble();
                         naturalLog(num);
@@ -63,7 +63,7 @@ public class Main {
                     case 4:
                         logger.info("Start of Execution");
                         System.out.println("You choose Power!");
-                        System.out.println("");
+                        System.out.println();
                         System.out.print("Enter Number: ");
                         num = reader.nextDouble();
                         System.out.print("Enter exponent: ");
@@ -82,34 +82,34 @@ public class Main {
     
     public static double squareRoot(double num) {
         double c = Math.sqrt(num);
-        System.out.println("");
+        System.out.println();
         System.out.println("The Result is " + c);
-        System.out.println("");
+        System.out.println();
         logger.info("Executing squareRoot function");
         return c;
     }
     public static int factorial(int num){
         int c = num;
         for(int i = num-1;i>=1;i--) c = c*i;
-        System.out.println("");
+        System.out.println();
         System.out.println("The Result is " + c);
-        System.out.println("");
+        System.out.println();
         logger.info("Executing factorial function");
         return c;
     }
     public static double naturalLog(double num){
         double c = Math.log(num);
-        System.out.println("");
+        System.out.println();
         System.out.println("The Result is " + c);
-        System.out.println("");
+        System.out.println();
         logger.info("Executing naturalLog function");
         return c;
     }
     public static double power(double num,double exp){
         double c = Math.pow(num,exp);
-        System.out.println("");
+        System.out.println();
         System.out.println("The Result is " + c);
-        System.out.println("");
+        System.out.println();
         logger.info("Executing power function");
         return c;
     }
