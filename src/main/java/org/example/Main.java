b731552aafb77f1acaf81f55347835179317fd5f
ManishReddyK <manish.koppula@iiitb.ac.in>
2023-11-14 14:36:51 +0530

    Modified the Code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -77,37 +77,4 @@ public class Main {
             }
         } while (flag == 0);
     }
-//    public static double squareRoot(double num) {
-//        double c = Math.sqrt(num);
-//        System.out.println();
-//        System.out.println("The Result is " + c);
-//        System.out.println();
-//        logger.info("Executing squareRoot function");
-//        return c;
-//    }
-//    public static int factorial(int num){
-//        int c = num;
-//        for(int i = num-1;i>=1;i--) c = c*i;
-//        System.out.println();
-//        System.out.println("The Result is " + c);
-//        System.out.println();
-//        logger.info("Executing factorial function");
-//        return c;
-//    }
-//    public static double naturalLog(double num){
-//        double c = Math.log(num);
-//        System.out.println();
-//        System.out.println("The Result is " + c);
-//        System.out.println();
-//        logger.info("Executing naturalLog function");
-//        return c;
-//    }
-//    public static double power(double num,double exp){
-//        double c = Math.pow(num,exp);
-//        System.out.println();
-//        System.out.println("The Result is " + c);
-//        System.out.println();
-//        logger.info("Executing power function");
-//        return c;
-//    }
 }
